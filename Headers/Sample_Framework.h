0ed325d837fba131eec2ab6db9de2cf3c92c1be9
xuanlinhdev <111721696+xuanlinhdev@users.noreply.github.com>
2023-03-17 16:00:20 +0700

    Initial commit

---
+++ Headers/Sample_Framework.h
@@ -0,0 +1,18 @@
+//
+//  Sample_Framework.h
+//  Sample Framework
+//
+//  Created by Xuân Linh on 17/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sample_Framework.
+FOUNDATION_EXPORT double Sample_FrameworkVersionNumber;
+
+//! Project version string for Sample_Framework.
+FOUNDATION_EXPORT const unsigned char Sample_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sample_Framework/PublicHeader.h>
+
+
